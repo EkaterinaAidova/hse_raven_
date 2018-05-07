d69b8a74b050fc9981672cf226115ec47a4bb426
JuliaRS <juliya.nnov@mail.ru>
2018-05-07 23:23:36 +0300

    Speech Recognition

--- HSE_Raven_Assistant/src/main/java/com/hse/raven/MainActivity.java
+++ HSE_Raven_Assistant/src/main/java/com/hse/raven/MainActivity.java
@@ -1,11 +1,20 @@
 package com.hse.raven;
 
+import android.content.ContentValues;
+import android.content.Entity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.Collections;
+import java.util.List;
 
 import ai.api.AIListener;
 import ai.api.*;
 import ai.api.android.AIConfiguration;
 import ai.api.android.AIService;
+import ai.api.model.EntityEntry;
+
+import static android.content.ContentValues.TAG;
 
 public class MainActivity extends UnityPlayerActivity implements AIListener {
 
@@ -19,17 +28,22 @@ public class MainActivity extends UnityPlayerActivity implements AIListener {
         rq.stopQueue();
 
         //AIConfiguration
-        final AIConfiguration config = new AIConfiguration(this.getResources().getString(R.string.CLIENT_ACESS_TOKEN),
+        final AIConfiguration config = new AIConfiguration(this.getResources().getString(R.string.CLIENT_ACCESS_TOKEN),
                 AIConfiguration.SupportedLanguages.Russian,
                 AIConfiguration.RecognitionEngine.System);
         aiService = AIService.getService(this, config);
         aiService.setListener(this);
+        //Entity hseGroups = new Entity("Groups");
+        //hseGroups.addEntry(new EntityEntry("15ПИ", new String[] {"пятнадцать п и", "пятнадцать пи"}));
+        //hseGroups.addEntry(new EntityEntry("15ПМИ", new String[] {"пятнадцать пэ мэ и","пятнадцать П М И"}));
+        //final List<Entity> entities = Collections.singletonList(hseGroups);
+        //RequestExtras requestExtras = new RequestExtras(null,entities);
         aiService.startListening();
     }
 
     @Override
     public void onResult(ai.api.model.AIResponse result) {
-
+        Log.i(TAG, "Result: " + result.getResult() );
     }
 
     @Override
