60dc755d6f92596c459409f36a037966444ad605
JuliaRS <juliya.nnov@mail.ru>
2018-05-07 22:09:32 +0300

    Speech Recognition

--- HSE_Raven_Assistant/src/main/java/com/hse/raven/MainActivity.java
+++ HSE_Raven_Assistant/src/main/java/com/hse/raven/MainActivity.java
@@ -2,15 +2,58 @@ package com.hse.raven;
 
 import android.os.Bundle;
 
-public class MainActivity extends UnityPlayerActivity {
+import ai.api.AIListener;
+import ai.api.*;
+import ai.api.android.AIConfiguration;
+import ai.api.android.AIService;
+
+public class MainActivity extends UnityPlayerActivity implements AIListener {
 
     protected RequestQueueSingleton rq;
-
+    protected AIService aiService;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         rq = RequestQueueSingleton.getInstance(this);
         rq.loadSchedule("2018.04.30", "2018.05.06", "15ПИ");
         rq.stopQueue();
+
+        //AIConfiguration
+        final AIConfiguration config = new AIConfiguration(this.getResources().getString(R.string.CLIENT_ACESS_TOKEN),
+                AIConfiguration.SupportedLanguages.Russian,
+                AIConfiguration.RecognitionEngine.System);
+        aiService = AIService.getService(this, config);
+        aiService.setListener(this);
+        aiService.startListening();
+    }
+
+    @Override
+    public void onResult(ai.api.model.AIResponse result) {
+
+    }
+
+    @Override
+    public void onError(ai.api.model.AIError error) {
+
+    }
+
+    @Override
+    public void onAudioLevel(float level) {
+
+    }
+
+    @Override
+    public void onListeningStarted() {
+
+    }
+
+    @Override
+    public void onListeningCanceled() {
+
+    }
+
+    @Override
+    public void onListeningFinished() {
+
     }
 }
