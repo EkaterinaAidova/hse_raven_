ff40e5484f63be7ce40479982955ce513c9b3442
EkaterinaAidova <aidova.ekaterina@gmail.com>
2018-05-10 01:06:04 +0300

    add corrcet view, delete stop queue and change in unity side for look up raven

--- HSE_Raven_Assistant/src/main/java/com/hse/raven/MainActivity.java
+++ HSE_Raven_Assistant/src/main/java/com/hse/raven/MainActivity.java
@@ -1,42 +1,38 @@
 package com.hse.raven;
 
-import android.Manifest;
-import android.content.ContentValues;
-import android.content.Entity;
-import android.content.pm.PackageManager;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.TextView;
 
 import com.google.gson.JsonElement;
+import com.unity3d.player.UnityPlayer;
 
-import java.util.Collections;
 import java.util.Date;
-import java.util.List;
 import java.util.Map;
 
 import ai.api.AIListener;
-import ai.api.*;
 import ai.api.android.AIConfiguration;
 import ai.api.android.AIService;
-import ai.api.model.EntityEntry;
 import ai.api.model.Result;
-
-import static android.content.ContentValues.TAG;
 
 public class MainActivity extends UnityPlayerActivity implements AIListener,View.OnTouchListener {
 
     protected RequestQueueSingleton rq;
     protected AIService aiService;
-    protected TextView tv;
+    SharedPreferences prefs;
+    Context appContext;
+    //protected TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        appContext = UnityPlayer.currentActivity.getApplicationContext();
+//PlayerPrefs: unity uses package name for preferences file ("com.example.app")
+        prefs = appContext.getSharedPreferences(appContext.getPackageName(), Context.MODE_PRIVATE);
         rq = RequestQueueSingleton.getInstance(this);
-       // rq.loadSchedule(new Date(),"15ПИ");
-       // rq.stopQueue();
+        mUnityPlayer.getView().setOnTouchListener(this);
         String aitoken = getResources().getString(R.string.CLIENT_ACCESS_TOKEN);
         //AIConfiguration
         final AIConfiguration config = new AIConfiguration(aitoken,
@@ -44,20 +40,24 @@ public class MainActivity extends UnityPlayerActivity implements AIListener,View
                 AIConfiguration.RecognitionEngine.System);
         aiService = AIService.getService(this, config);
         aiService.setListener(this);
-        // set OnTouchListener
-        View view = mUnityPlayer.getView();
-        view.setOnTouchListener(this);
-        //tv = new TextView(this); // for debug
-        //tv.setOnTouchListener(this); // for debug
-        //setContentView(tv);
     }
 
 
     @Override
     public boolean onTouch(View view, MotionEvent event) {
         switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN: // нажатие
-                aiService.startListening();
+            case MotionEvent.ACTION_DOWN: //
+                appContext = UnityPlayer.currentActivity.getApplicationContext();
+//PlayerPrefs: unity uses package name for preferences file ("com.example.app")
+                prefs = appContext.getSharedPreferences(appContext.getPackageName(), Context.MODE_PRIVATE);// нажатие
+                Map<String, ?>pr = prefs.getAll();
+                Log.i("unityprefs", pr.toString());
+              /*  if() {
+                    Boolean onRaven = mUnityPlayer.getSettings().getBoolean("onRaven");
+                    if (onRaven) {*/
+                        aiService.startListening();
+                    /*}
+                }*/
                 break;
             case MotionEvent.ACTION_UP: // отпускание
                 aiService.stopListening();
@@ -119,6 +119,6 @@ public class MainActivity extends UnityPlayerActivity implements AIListener,View
         group = group.toUpperCase();
         //System.out.println(group); // for debug
         rq.loadSchedule(new Date(),group);
-        rq.stopQueue();
+     //   rq.stopQueue();
     }
 }
